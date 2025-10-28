25a1361f70e5d19136e4cedb854976a8cda300f2
Arvid Berlin <arvidberlinjobb@gmail.com>
2025-10-29 00:55:39 +0100

    Added login page, with form to login, connected to same blocklist as sign up

--- Models/Section.generated.cs
+++ Models/Section.generated.cs
@@ -50,6 +50,13 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 		// properties
 
 		///<summary>
+		/// Max Articles
+		///</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.3.2+8aa9dc8")]
+		[ImplementPropertyType("maxArticles")]
+		public virtual int MaxArticles => this.Value<int>(_publishedValueFallback, "maxArticles");
+
+		///<summary>
 		/// Section Background Color
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "16.3.2+8aa9dc8")]
