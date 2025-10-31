5a54b39b9d39ecb805a9910df2d70b1c963e398b
Benjamin Rasoli (student syh) <benjamin.rasoli@yh.nackademin.se>
2025-10-31 09:57:24 +0100

    Updated UI for Exercise Card

--- Business/Dto/ExerciseDto.cs
+++ Business/Dto/ExerciseDto.cs
@@ -2,9 +2,14 @@
 
 public class ExerciseDto
 {
+    public string? ExerciseId { get; set; }
     public string? Name { get; set; }
-    public string? BodyPart { get; set; } 
-    public string? Target { get; set; } 
-    public string? Equipment { get; set; }
+
+    public List<string>? BodyParts { get; set; }
+    public List<string>? TargetMuscles { get; set; }
+    public List<string>? Equipments { get; set; }
+    public List<string>? SecondaryMuscles { get; set; }
+    public List<string>? Instructions { get; set; }
+
     public string? GifUrl { get; set; }
 }
