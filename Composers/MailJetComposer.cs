3f1ba6bc9e314d9609caf6726fe14b23dca5f246
Benjamin Rasoli (student syh) <benjamin.rasoli@yh.nackademin.se>
2025-11-26 19:29:50 +0100

    Added umbraco form in footer for news letter

---
+++ Composers/MailJetComposer.cs
@@ -0,0 +1,13 @@
+﻿using Umbraco.Cms.Core.Composing;
+using Umbraco.Forms.Core.Providers;
+using WorkoutProgramBuilder.Workflows;
+
+namespace WorkoutProgramBuilder.Composers;
+
+public class MailJetComposer : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        builder.WithCollectionBuilder<WorkflowCollectionBuilder>().Add<MailJetWorkflow>();
+    }
+}
